bc7d1310ebece0a7e847e9dd0d29cf296c2eb188
LeeJeongSeok <ljs0429777@gmail.com>
2020-04-29 19:11:45 +0900

    CrawlingControllerTest testcode 작성중

---
+++ src/main/java/com/lee/practice/crawling/web/CrawlingController.java
@@ -0,0 +1,32 @@
+package com.lee.practice.crawling.web;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class CrawlingController {
+
+    @GetMapping("/")
+    public String index() {
+        return  "crawling";
+    }
+
+    @GetMapping("/crawling")
+    public String crawling() throws IOException {
+
+        Document document = Jsoup.connect("https://www.naver.com/").get();
+
+        if (document != null) {
+            return "connect";
+        } else {
+            return "connect fail";
+        }
+
+    }
+}
---
+++ src/test/java/com/lee/practice/crawling/web/CrawlingControllerTest.java
@@ -0,0 +1,56 @@
+package com.lee.practice.crawling.web;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = CrawlingController.class)
+public class CrawlingControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void crawling_controller가_리턴된다() throws Exception {
+
+        String text = "crawling";
+
+        mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(text));
+    }
+
+    @Test
+    public void crawling할려는_웹페이지가_정상작동중인가() throws Exception {
+
+        Document document = Jsoup.connect("https://www.naver.com/").get();
+
+        mvc.perform(get("/crawling")).andExpect(status().isOk()).andExpect(content().string("connect"));
+    }
+
+    @Test
+    public void crawling할려는_태그가_있는가() throws Exception {
+
+        Document document = Jsoup.connect("https://www.naver.com/").get();
+
+        Elements elements = document.select("body");
+
+    }
+
+    @Test
+    public void 태그안에_data가_있는가() throws Exception {
+
+    }
+
+}
