ee66e69572d3117d5c332b8ee4b3f44189b62964
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-01 12:57:30 +0900

    production code 개선

--- src/main/java/com/lee/practice/crawling/service/RestaurantService.java
+++ src/main/java/com/lee/practice/crawling/service/RestaurantService.java
@@ -3,39 +3,40 @@ package com.lee.practice.crawling.service;
 import com.lee.practice.crawling.dto.Menu;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Service
 public class RestaurantService {
 
-    Document document;
-
-
-    public void callUrl() throws Exception{
-        document = Jsoup.connect("http://snuco.snu.ac.kr/ko/foodmenu").get();
-    }
-
-    public void data__crawling() {
-        Elements tdElement = document.select("table tbody tr td");
-
-
-        Menu menu = Menu.builder()
-                .restaurant(tdElement.get(0).text())
-                .breakfast(tdElement.get(1).text())
-                .lunch(tdElement.get(2).text())
-                .dinner(tdElement.get(3).text())
-                .build();
-    }
-
-    public void add_list() {
+    public void crawling_data() throws Exception {
 
         List<Menu> menuList = new ArrayList<>();
 
-    }
+        Document document = Jsoup.connect("http://snuco.snu.ac.kr/ko/foodmenu").get();
+        Elements tdElement = document.select("table tbody tr");
 
+        for (Element content : tdElement) {
+            Elements tdContent = content.select("td");
+
+            Menu menu = Menu.builder()
+                    .restaurant(tdContent.get(0).text())
+                    .breakfast(tdContent.get(1).text())
+                    .lunch(tdContent.get(2).text())
+                    .dinner(tdContent.get(3).text())
+                    .build();
+
+            menuList.add(menu);
+
+        }
+
+        System.out.println(menuList.get(4));
+
+    }
 
 }
--- src/main/java/com/lee/practice/crawling/web/CrawlingController.java
+++ src/main/java/com/lee/practice/crawling/web/CrawlingController.java
@@ -1,5 +1,6 @@
 package com.lee.practice.crawling.web;
 
+import com.lee.practice.crawling.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,17 @@ import java.io.IOException;
 @RestController
 public class CrawlingController {
 
+    @Autowired
+    RestaurantService restaurantService;
+
 
     @GetMapping("/crawling")
-    public String crawling() throws IOException {
-
-
+    public String crawling() {
+        try {
+            restaurantService.crawling_data();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "crawling";
     }
 }
